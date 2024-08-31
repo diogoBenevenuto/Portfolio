029f69fc5ba8dbea594c5f97a5c30ff1a61244a8
diogoBenevenuto <diogommonteiro123@gmail.com>
2024-08-31 19:02:31 -0300

    Adjust for Contact me to work, adding codes in the Script and some libraries in the index

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
+const contactForm = document.querySelector('#contactForm');
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
+const fullName = document.getElementById("fullname");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail()
+{
+    const bodyMessage = `Full name: ${fullName.value}<br> Email: ${email.value}<br> 
+    Phone Number: ${number.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        SecureToken : "be87cd1f-905a-4e50-aeae-58ae21bc60fc",
+        To : 'diogommonteiro123@gmail.com',
+        From : "maoridev9@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK"){
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+
+    contactForm.reset();
+    
+})
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-//tudo novo abaixo
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    var fullname = document.getElementById("fullname").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("number").value;
-    var subject = document.getElementById("subject").value;
-    var message = document.getElementById("message").value;
-
-    var data = {
-        fullname: fullname,
-        email: email,
-        phone: phone,
-        subject: subject,
-        message: message
-    };
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "send_email.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                alert("Mensagem enviada com sucesso!");
-
-                document.getElementById("contactForm").reset();
-            } else {
-                alert("Erro ao enviar a mensagem.");
-            }
-        }
-    };
-
-
-    xhr.send(JSON.stringify(data));
-});
